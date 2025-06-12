961fac932b4ce9b52630386104a7597dc910fd0f
Pauline Moraa <pmoraa431@gmail.com>
2025-06-12 03:20:29 +0300

    Use Flask-SQLAlchemy to define a data model Use SQLAlchemy-Serializer to serialize an object Implement a Flask application that queries the database Implement a Flask application that returns a JSON response

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,6 +21,24 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if not earthquake:
+        return make_response(jsonify({"message": f"Earthquake {id} not found."}), 404)
+    return make_response(earthquake.to_dict(), 200)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes_data = [quake.to_dict() for quake in earthquakes]
+    return make_response(
+
+        {
+        "count": len(earthquakes),
+        "quakes": quakes_data
+        }, 200
+    )
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,15 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+#Add a __repr__ method to return a string that formats the attributes id, magnitude, location, and year as a comma-separated sequence as shown:
+#<Earthquake 1, 9.5, Chile, 1960>
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
